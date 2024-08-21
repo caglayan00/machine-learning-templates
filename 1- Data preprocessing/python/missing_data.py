3e1c439c1dddefbb3235ad38ba873bd84786aad2
caglayan00 <caglayan_aslan@hotmail.com>
2024-08-21 13:05:35 +0300

    Update missing_data.py

--- 1- Data preprocessing/python/missing_data.py
+++ 1- Data preprocessing/python/missing_data.py
@@ -1,18 +1,24 @@
-# Missing data
-#clen up missing data (NaN) using the mean
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
 
-# Taking care of missing data
+# Separating independent variables (X) and dependent variable (y)
+x = dataset.iloc[:, :-1].values  # Independent variables
+y = dataset.iloc[:, -1].values   # Dependent variable
+
+# Handling missing data using SimpleImputer
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Define the imputer to replace missing values with the mean of the column
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+# Fit the imputer to the appropriate columns (assuming columns 1 and 2 have missing data)
+imputer = imputer.fit(x[:, 1:3])
+
+# Transform the data to fill in the missing values
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Now `x` and `y` are ready for further processing or model training
