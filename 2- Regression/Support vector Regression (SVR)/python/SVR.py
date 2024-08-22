50050c17b7a4fc32b4f6c306da5cf779bf7d3eef
caglayan00 <caglayan_aslan@hotmail.com>
2024-08-22 17:02:27 +0300

    Update SVR.py

--- 2- Regression/Support vector Regression (SVR)/python/SVR.py
+++ 2- Regression/Support vector Regression (SVR)/python/SVR.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 28 17:20:35 2017
-
-SVR - Support Vector Machine for regression
-
-@author: Ilaria
-"""
-
-
-# Data Preprocessing Template
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,46 +8,26 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-#we don't apply training and test set because we don't have enough data
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-"""
-
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
+
 X = sc_X.fit_transform(X)
-y= sc_y.fit_transform(y)
+y = sc_y.fit_transform(y.reshape(-1, 1))  # Reshape y to a 2D array
 
-#fitting  SVR model to the dataset
+# Fitting SVR to the dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
-regressor.fit(X,y)
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y.ravel())  # Flatten y to fit the model
 
+# Predicting a new result with SVR
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))).reshape(-1, 1))
 
-
-
-#Predicting a new result with SVR
-y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
-
-
-#visualize SVR results
-plt.scatter(X, y, color= 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Reality vs Bluff(SVR regression)')
-plt.xlabel('Position label')
+# Visualizing the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)), color='blue')
+plt.title('Reality vs Bluff (SVR Regression)')
+plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
